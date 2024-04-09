5dff95ebacb7cbad41c63e21e412f3cc28166ae9
camitalla <157500162+camitalla@users.noreply.github.com>
2024-04-09 17:48:04 -0400

    Putting in landing page info and basic Javascript

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,23 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+//Code on how to make accordion menu taken from w3schools.com (https://www.w3schools.com/howto/howto_js_accordion.asp)
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
